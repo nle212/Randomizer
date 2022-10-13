db5b681e20c8e449f4273c70816f0e22c076dd91
nle212 <lenguyen8555@gmail.com>
2022-10-13 15:28:53 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,6 +1,20 @@
-let servants = [ Arjuna, Karna, Kama, Muramasa, Oberon, Vitch];
+let servants = [{
+  name :'Arjuna', 
+  alignment : 'lawful evil'
+}, {
+  name: 'Karna',
+  alignment: 'lawful good'
+} ,{
+  name:'Kama', 
+  alignment :'chaotic evil'
+}, {
+  name :'Oberon',
+  alignment:'chaotic evil'
+}];
 function setup() {
   createCanvas(400, 400);
+
+  console.log(servants[1]);
 }
 
 function draw() {
