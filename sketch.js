b55ab7011ef114d0037471494cce852affaeceea
nle212 <lenguyen8555@gmail.com>
2022-10-13 17:23:24 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,12 +20,14 @@ background(200);
 }
 
 function draw() {
-
+  
 }
 
 function mousePressed(){
+  if (servants[0]){
   background(random(200,255));
   randomIndex = int(random(servants.length));
   text(servants[randomIndex].name, 50, 50);
   servants.splice(randomIndex,1);
+}
 }
